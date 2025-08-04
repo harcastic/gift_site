f7476c1956d24016514e18abcd62d6f4375a89fe
Harsh Raj <143740143+vellaharsh@users.noreply.github.com>
2025-08-04 21:55:00 +0530

    nav-bar
    made nav-bar

---
+++ home.js
@@ -0,0 +1,12 @@
+  const searchIcon = document.getElementById('search-icon');
+  const searchBar = document.getElementById('search-bar');
+
+  searchIcon.addEventListener('click', () => {
+    searchBar.classList.toggle('active');
+  });
+  document.addEventListener('click', (event) => {
+  if (!searchBar.contains(event.target) && event.target !== searchIcon) {
+    searchBar.classList.remove('active');
+  }
+});
+
